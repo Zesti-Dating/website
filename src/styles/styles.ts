b0f599e84488d76cb19fb3509b3c999fafcc90ff
Steve Li <lithafnium2000@gmail.com>
2021-04-15 00:37:02 -0400

    initial commit

---
+++ src/styles/styles.ts
@@ -0,0 +1,9 @@
+export const colors = {
+  PRIMARY: "#FD8440",
+  TERNARY: "#FFF6E9",
+};
+
+export const fonts = {
+  PRIMARY: `-apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto, Ubuntu;`,
+  SECONDARY: "'Poppins', sans-serif",
+};
