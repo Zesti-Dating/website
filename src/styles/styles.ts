aa29959e34ae40b3c275dfa9f81555b2ef17ae57
Steve Li <lithafnium2000@gmail.com>
2021-04-16 22:26:32 -0400

    sliding animations

--- src/styles/styles.ts
+++
@@ -1,9 +0,0 @@
-export const colors = {
-  PRIMARY: "#FD8440",
-  TERNARY: "#FFF6E9",
-};
-
-export const fonts = {
-  PRIMARY: `-apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto, Ubuntu;`,
-  SECONDARY: "'Poppins', sans-serif",
-};
