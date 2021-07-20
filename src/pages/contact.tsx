a7ffdf2013ccbc0431c4982e40eafa83de92474b
Leon Chen <lrchen01@gmail.com>
2021-07-20 01:19:49 -0400

    forms 13

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -29,33 +29,49 @@ const Contact = () => {
    * TODO: Create inputs, rows with content
    */
   return (
-    <form name="contact" method="POST" data-netlify="true">
-      <input type="hidden" name="form-name" value="contact" />
-      <p>
-        <label>Your Name: <Input type="text" name="name"/></label>
-      </p>
-      <p>
-        <label>Your Email: <Input type="email" name="email"/></label>
-      </p>
-      <p>
-        <label>Message: <InputArea name="message"></InputArea></label>
-      </p>
-      <p>
-        <Button 
-          type="submit"
-          margin="20px"
-          backgroundColor={colors.PRIMARY}
-          padding="10px 10px 10px 10px"
-          borderRadius="40px"
-          color="#ffffff"
-          style={{
-            width: "200px",
-          }}
-        >
-          Join Waitlist
-        </Button>
-      </p>
-    </form>
+    <>
+      <Container>
+        <Banner />
+        <Navbar />
+        <ZestiFadeIn>
+          <div
+            style={{ width: "100%", display: "flex", justifyContent: "center" }}
+          >
+            <ContainerInner>
+              <ZestiSlideIn left>
+                <form name="contact" method="POST" data-netlify="true">
+                  <input type="hidden" name="form-name" value="contact" />
+                  <p>
+                    <label>Your Name: <Input type="text" name="name"/></label>
+                  </p>
+                  <p>
+                    <label>Your Email: <Input type="email" name="email"/></label>
+                  </p>
+                  <p>
+                    <label>Message: <InputArea name="message"></InputArea></label>
+                  </p>
+                  <p>
+                    <Button 
+                      type="submit"
+                      margin="20px"
+                      backgroundColor={colors.PRIMARY}
+                      padding="10px 10px 10px 10px"
+                      borderRadius="40px"
+                      color="#ffffff"
+                      style={{
+                        width: "200px",
+                      }}
+                    >
+                      Join Waitlist
+                    </Button>
+                  </p>
+                </form>
+              </ZestiSlideIn>
+            </ContainerInner>
+          </div>
+        </ZestiFadeIn>
+      </Container>
+    </>
   );
 };
 
