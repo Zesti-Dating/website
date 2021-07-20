637fb39f6bc7addfa30761582e56577775396e0f
Leon Chen <lrchen01@gmail.com>
2021-07-20 11:16:07 -0400

    flex test 8

--- src/shared/components/navbar/navbar.tsx
+++ src/shared/components/navbar/navbar.tsx
@@ -6,11 +6,13 @@ import { colors } from '@app/styles/styles'
 import routes from '@app/shared/constants/routes'
 import Zesti from "@app/assets/zesti.svg"
 import Instagram from "@app/assets/instagram.svg";
+import Harvard from "@app/assets/harvard.svg";
 const Navbar = () => {
   return (
     <NavbarContainer>
       <NavbarInner>
         <Image height="150px" src={Zesti} />
+        <Image height="150px" src={Harvard} />
         <Links>
           <NavItem>
             <Link to={routes.INSTALINK}>
