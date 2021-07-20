048ffe34cd8437370d2ea86115209f06acfe0e2e
Leon Chen <lrchen01@gmail.com>
2021-07-20 01:08:51 -0400

    forms 11

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -32,10 +32,10 @@ const Contact = () => {
     <form name="contact" method="POST" data-netlify="true">
       <input type="hidden" name="form-name" value="contact" />
       <p>
-        <label>Your Name: <input type="text" name="name"/></label>
+        <label>Your Name: <Input type="text" name="name"/></label>
       </p>
       <p>
-        <label>Your Email: <input type="email" name="email"/></label>
+        <label>Your Email: <Input type="email" name="email"/></label>
       </p>
       <p>
         <label>Message: <textarea name="message"></textarea></label>
